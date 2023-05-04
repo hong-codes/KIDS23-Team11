be49f9f55de76b7e8bc5f61ac661bc651aa9622a
Aman Patel <amanpatel@gmail.com>
2023-05-04 14:21:54 -0500

    question page has text area and form and a table

--- frontend/src/views/Questions.vue
+++ frontend/src/views/Questions.vue
@@ -3,10 +3,93 @@
     <div class="row">
       <div class="col-12">
         <h1>Submit Questions</h1>
+
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">Your Question</label>
+          <textarea
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+            placeholder="Your question goes here"
+          ></textarea>
+        </div>
+        <button class="btn btn-primary mb-3">Submit</button>
       </div>
-      <pre>
-      {{ questions }}
-      </pre>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Question</th>
+              <th scope="col">Answers</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>What is the meaning of life?</td>
+              <td>
+                <ul>
+                  <li>Answer 1</li>
+                  <li>Answer 2</li>
+                  <li>Answer 3</li>
+                </ul>
+              </td>
+            </tr>
+            <tr>
+              <td>What is the meaning of life?</td>
+              <td>
+                <ul>
+                  <li>Answer 1</li>
+                  <li>Answer 2</li>
+                  <li>Answer 3</li>
+                </ul>
+              </td>
+            </tr>
+            <tr>
+              <td>What is the meaning of life?</td>
+              <td>
+                <ul>
+                  <li>Answer 1</li>
+                  <li>Answer 2</li>
+                  <li>Answer 3</li>
+                </ul>
+              </td>
+            </tr>
+            <tr>
+              <td>What is the meaning of life?</td>
+              <td>
+                <ul>
+                  <li>Answer 1</li>
+                  <li>Answer 2</li>
+                  <li>Answer 3</li>
+                </ul>
+              </td>
+            </tr>
+            <tr>
+              <td>What is the meaning of life?</td>
+              <td>
+                <ul>
+                  <li>Answer 1</li>
+                  <li>Answer 2</li>
+                  <li>Answer 3</li>
+                </ul>
+              </td>
+            </tr>
+            <tr>
+              <td>What is the meaning of life?</td>
+              <td>
+                <ul>
+                  <li>Answer 1</li>
+                  <li>Answer 2</li>
+                  <li>Answer 3</li>
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
       <div class="col-4">
         <QuestionList :questions="questions" />
       </div>
